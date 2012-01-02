6eca8227bc68c14b294783f924132c543feaa29a
Dalton Barreto <daltonmatos@gmail.com>
2012-01-02 01:35:57 -0200

    Calculo correto do MD5

--- main/models.py
+++ main/models.py
@@ -5,13 +5,15 @@ from django.db import models
 from hashlib import md5
 from django.conf import settings
 import os
+from django.core.files.storage import FileSystemStorage
 
 def _upload_path(instance, filename):
     basepath = settings.UPLOAD_BASE_PATH
-    return os.path.join(basepath, md5().hexdigest())
+    return os.path.join(basepath, md5(filename).hexdigest())
 
 
 class File(models.Model):
     name = models.CharField(max_length=256)
     title = models.TextField()
-    content = models.FileField(upload_to = _upload_path)
+    content = models.FileField(upload_to = _upload_path, \
+            storage = FileSystemStorage(location = settings.UPLOAD_BASE_PATH))
