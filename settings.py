a8d4ae318b9aa59c76e6cb61a153892ca8fd8503
Dalton Barreto <daltonmatos@gmail.com>
2012-04-11 23:21:09 -0300

    Code cleaning

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ ROOT = os.path.dirname(os.path.abspath(__file__))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': os.path.join(ROOT, 'db.sqlite3'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'registration',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
@@ -151,4 +152,3 @@ LOGGING = {
 }
 
 UPLOAD_BASE_PATH = os.path.join(ROOT, 'uploads')
-
