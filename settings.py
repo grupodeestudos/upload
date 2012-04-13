e3f99c990757b51acd75f2270192d7ac4ac7fcfc
Dalton Barreto <daltonmatos@gmail.com>
2012-04-13 16:20:36 -0300

    Remove registrarion app

--- settings.py
+++ settings.py
@@ -121,7 +121,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'registration',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
